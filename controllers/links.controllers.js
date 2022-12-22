bd241ba6f407a7bdcecb6f13385cd42f21a7b8a8
MolinaMS95 <matheusmolina95@gmail.com>
2022-12-22 17:50:16 -0300

    feat: route get urls/:id

--- controllers/links.controllers.js
+++ controllers/links.controllers.js
@@ -8,10 +8,29 @@ export async function shortenLink(req, res) {
 
   try {
     await connectionDB.query(
-      "INSERT INTO links (user_id, url, shortUrl) VALUES ($1, $2, $3);",
+      `INSERT INTO links (user_id, url, "shortUrl") VALUES ($1, $2, $3);`,
       [user.id, url.url, shortUrl]
     );
     res.status(201).send({ shortUrl });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function getLinkById(req, res) {
+  const { id } = req.params;
+
+  try {
+    const { rows } = await connectionDB.query(
+      `SELECT id, "shortUrl", url FROM links WHERE id=$1;`,
+      [id]
+    );
+
+    if (rows.length === 0) {
+      return res.sendStatus(404);
+    }
+
+    return res.status(200).send(rows[0]);
   } catch (error) {
     res.status(500).send(error.message);
   }
--- routes/links.routes.js
+++ routes/links.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { urlSchemaValidation } from "../middlewares/urlSchemaValidation.middleware.js";
 import { userTokenValidation } from "../middlewares/userTokenValidation.middleware.js";
-import { shortenLink } from "../controllers/links.controllers.js";
+import { shortenLink, getLinkById } from "../controllers/links.controllers.js";
 
 const router = Router();
 
 router.post("/urls/shorten", urlSchemaValidation, userTokenValidation, shortenLink);
+router.get("/urls/:id", getLinkById);
 
 export default router;
